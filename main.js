9516c0d5aed06cf2c9213e89ddddb6b188dc2941
Wiberg <klutten99@gmail.com>
2020-09-23 00:03:33 +0200

    Update

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ function getAllItems() {
 
 // Main function (Called when "Get Prices" button is pressed)
 function getPrices() {
+    if(location.hostname != "www.albionflipper.ml" && location.hostname != "albionflipper.ml")
+      return;
     tier = document.getElementById("tier").value;
     enchantment = document.getElementById("enchantment").value;
     quality = document.getElementById("quality").value;
